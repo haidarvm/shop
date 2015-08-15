1e31a8f7cb51dba3ca2c87fe9ecf9a7dc46cfa02
haidarvm <haidarvm@gmail.com>
2015-08-15 23:31:49 +0700

    cleaning

--- application/shop/controllers/admin/Product.php
+++ application/shop/controllers/admin/Product.php
@@ -16,27 +16,42 @@ class Product extends MY_Controller {
 
     public function create() {
         $data['title'] = "Admin Create Product";
+        $data['productDraft'] = $this->mproduct->getProductDraft();
+        if (! $data['productDraft']) {
+            $dataDraft['name'] = 'draft';
+            $insertProductId = $this->mproduct->insertQuickProduct($dataDraft);
+            $dataImgDraft['product_id'] = $insertProductId;
+            $dataImgDraft['name'] = 'draft';
+            $insertProductImageId = $this->mproduct->insertQuickProductImg($dataImgDraft);
+            $data['productDraft'] = $this->mproduct->getLatestProductDraft($insertProductId);
+            $data['productImgDraft'] = $this->mproduct->getLatestProductImgDraft($insertProductId);
+        }
+        $data['productImgDraft'] = $this->mproduct->getProductImgDraft($data['productDraft']->product_id);
         $data['categories'] = $this->mcategory->getAllCategories();
-        $data['error'] = "";
-        $this->load->view('admin/product', $data);
+        $this->load->admin_template('admin/dashboard', $data);
     }
 
-    public function do_upload() {
-        $config['upload_path'] = FCPATH.'/assets/files/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['file_name'] = 'good.jpg';
-        $config['overwrite'] = false;
-        $config['max_size'] = 100000;
-        $config['max_width'] = 100024;
-        $config['max_height'] = 76800;
-        $this->load->library('upload', $config);
-        if (! $this->upload->do_upload($field = 'filed')) {
-            $error = array('error' => $this->upload->display_errors());
-            $this->load->view('admin/product', $error);
-        } else {
-            $data = array('upload_data' => $this->upload->data());
-            $this->load->view('admin/product_success', $data);
-        }
+    public function insert_quick_product() {
+        $post = $this->input->post();
+        unset($post['_wysihtml5_mode']);
+        // print_r($post);
+        $post['slug'] = $this->checkSlug($post['name']);
+        $this->mproduct->editProduct($post, $post['product_id']);
+        // echo json_encode($post);
+        $this->insert_draft_product();
     }
+    
+    public function insert_draft_product() {
+        $post = $this->input->post();
+        $dataDraft['name'] = 'draft';
+        $product_id = $this->mproduct->insertQuickProduct($dataDraft);
+        $dataImgDraft['product_id'] = $product_id;
+        $dataImgDraft['name'] = 'draft';
+        $image_id = $this->mproduct->insertQuickProductImg($dataImgDraft);
+        $data['product_id'] = $product_id;
+        $data['image_id'] = $image_id;
+        echo json_encode($data);
+    }
+    
 
 }
--- application/shop/views/admin/product.php
+++ application/shop/views/admin/product.php
@@ -1,20 +1,89 @@
-<html>
-<head>
-<title>Upload Form</title>
-</head>
-<body>
 
-<?php echo $error;?>
-
-<?php echo form_open_multipart('admin/product/do_upload');?>
-
-<input type="file" name="filed" size="20" />
-
-<br /><br />
-
-<input type="submit" value="upload" />
-
-</form>
-
-</body>
-</html>
+<!-- Quick New Product -->
+<div class="box box-info" id="quick-product-box">
+	<div class="box-header">
+		<i class="fa fa-envelope"></i>
+		<h3 class="box-title">Quick New Product</h3>
+		<!-- tools box -->
+		<div class="pull-right box-tools">
+			<button class="btn btn-info btn-sm" data-toggle="tooltip"
+				title="Add More">
+				<i class="fa fa-plus"></i>
+			</button>
+		</div>
+		<!-- /. tools -->
+	</div>
+	<div class="box-body">
+		<form action="#" class="form-horizontal" id="quick-product"
+			method="post">
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="name">Product</label>
+				<div class="col-sm-10">
+					<input id="name" name="name" class="form-control" type="text"
+						placeholder="Name">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="name">Category</label>
+				<div class="col-sm-10">
+					<select name="category_id" class="form-control">
+									<?php foreach($categories as $cat) {?>
+										<option value="<?php echo $cat->category_id;?>"><?php echo $cat->name;?></option>
+									<?php }?>
+									</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="name">Price</label>
+				<div class="col-sm-10">
+					<input id="name" name="price" class="form-control numbers"
+						type="text" placeholder="Rp">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="name">Description</label>
+				<div class="col-sm-10">
+					<textarea name="description" class="textarea"
+						placeholder="Description"
+						style="width: 100%; height: 125px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="name">Stock</label>
+				<div class="col-sm-10">
+					<input id="name" name="stock" class="form-control number"
+						type="text" placeholder="Qty">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="image">Image</label>
+				<div class="col-sm-10">
+					<span class="btn btn-success fileinput-button"> <i
+						class="glyphicon glyphicon-plus"></i> <span>Add files...</span> <!-- The file input field used as target for the file upload widget -->
+						<input id="fileupload" type="file" name="userfile" multiple> <input
+						type="hidden" name="product_id" id="product_id"
+						value="<?php echo !empty($productDraft) ? $productDraft->product_id : "";?>">
+						<input type="hidden" name="image_id" id="image_id"
+						value="<?php echo !empty($productImgDraft) ? $productImgDraft->image_id : "";?>">
+					</span>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="image">Progress</label>
+				<div class="col-sm-10">
+					<!-- The global progress bar -->
+					<div id="progress" class="progress">
+						<div class="progress-bar progress-bar-success"></div>
+					</div>
+					<!-- The container for the uploaded files -->
+					<div id="files" class="files"></div>
+				</div>
+			</div>
+		</form>
+	</div>
+	<div class="box-footer clearfix">
+		<button class="pull-right btn btn-default" id="submit">
+			Submit <i class="fa fa-arrow-circle-right"></i>
+		</button>
+	</div>
+</div>
